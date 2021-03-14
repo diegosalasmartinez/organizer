6dda70e7786dbeae097e03e10e4aba3627a57f91
diegosalasmartinez <diesalasmart@gmail.com>
2021-03-13 19:23:35 -0500

    gh pages not working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { HashRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Login from "./components/login"
