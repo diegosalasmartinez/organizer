331a9a57594612c8a6b894b2fa06bfa900d6b5a5
diegosalasmartinez <diesalasmart@gmail.com>
2021-03-15 02:12:23 -0500

    edit task page added

--- src/components/CreateTask.js
+++ src/components/CreateTask.js
@@ -8,39 +8,11 @@ require('dotenv').config();
 const cookies = new Cookies();
 
 export default class CreateTask extends Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            name: '',
-            description: '',
-            duration: 60,
-            importance: 1,
-            registration_date: new Date(),
-            due_date: new Date()
-        }
-    }
-
     componentDidMount() {
         if(!cookies.get('username')){
             window.location.href="./";
         }
-        if(this.props.id){
-            console.log('ESTAMOS EN EDITAR');
-        }
-        else{
-            console.log('ESTAMOS EN CREAR');
-        }
     }
-
-    handleChange = ({target}) => {
-        const {name, value} = target;
-        this.setState({[name]: value, userFailed: false});
-    }
-
-    onChangeDate = (date)=>{
-        this.setState({due_date: date});
-    }
-
     createTask = (newTask) => {
         axios.post(`${process.env.REACT_APP_API}/tasks/add`,newTask)
             .then(res => {
--- src/components/EditTask.js
+++ src/components/EditTask.js
@@ -1,7 +1,10 @@
 import React, { Component } from 'react'
-import CreateTask from './CreateTask'
+import axios from 'axios';
+import Navbar from "./Navbar"
+import TaskForm from "./TaskForm"
 import Cookies from 'universal-cookie';
 
+require('dotenv').config();
 const cookies = new Cookies();
 
 export default class EditTask extends Component {
@@ -11,9 +14,23 @@ export default class EditTask extends Component {
         }
     }
 
+    updateTask = (newTask) => {
+        console.log('UPDATE TASK');
+        // axios.post(`${process.env.REACT_APP_API}/tasks/add`,newTask)
+        //     .then(res => {
+        //         console.log(res.data);
+        //         window.location.href="./";
+        //     })
+        //     .catch(e => console.log(e));
+    }
+
     render() {
         return (
-            <CreateTask id={this.props.match.params.id}/>
+            <div className="mainPage">
+                <Navbar />
+                <br></br>
+                <TaskForm onSubmit={this.updateTask} textButton={"Update Task"}/>
+            </div>
         )
     }
 }
